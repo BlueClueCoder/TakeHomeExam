10abace0bad903bf5e5fbbb3f7f4b3b91ae6d696
BlueClueCoder <elliott@januzelli.net>
2026-03-15 12:00:15 -0400

    Ability to modify subs and users, removed unnecessary homepage comps, update datatypes

--- app/index.tsx
+++ app/index.tsx
@@ -2,21 +2,10 @@ import { useNavigation } from "expo-router";
 import { useContext } from "react";
 import { Button, Text, View } from "react-native";
 import { UserSubscriptionContext } from "./contexts/UserSubscriptionContext";
-import { IUser } from "./datatypes/IUser";
 
 export default function Index() {
   const UserSubscriptionInfo = useContext(UserSubscriptionContext);
   const navigator = useNavigation();
-
-  function _updateList() {
-    const newUsers: IUser[] = UserSubscriptionInfo.users.splice(0);
-    if (newUsers[0].userName === "Pablo") {
-      newUsers[0].userName = "Jimmy";
-    } else {
-      newUsers[0].userName = "Pablo";
-    }
-    UserSubscriptionInfo.changeUsers(newUsers);
-  }
 
   return (
     <View
@@ -27,8 +16,6 @@ export default function Index() {
       }}
     >
       <Text>This is the Home Page.</Text>
-      <Text>{UserSubscriptionInfo.users[0].userName}</Text>
-      <Button title="Click to Change" onPress={() => _updateList()}></Button>
       <Button onPress={() => navigator.navigate("Users")} title="View Users"></Button>
       <Button onPress={() => navigator.navigate("Subscriptions")} title="View Subscriptions"></Button>
     </View>
